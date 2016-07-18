49ac99ef80bedc092fc7ac1bb09ad0ba175c773b
team vwyf <philetus@gmail.com>
2016-07-18 05:22:38 +0000

    added led for demo

--- light_demon.py
+++ light_demon.py
@@ -1,16 +1,20 @@
 import RPi.GPIO as GPIO
 
-sPin = 17 # GPIO17
+sPin = 17 # GPIO17 - sensor
+lPin = 27 # GPIO27 - led
 
 # pin setup
 GPIO.setmode(GPIO.BCM) # Broadcom pin-numbering scheme
 GPIO.setup(sPin, GPIO.IN)
+GPIO.setup(lPin, GPIO.OUT)
 
 try:
-    while 1:
-        if GPIO.input(sPin): # button is released
-            print("pew pew")
-        else: # button is pressed:
-            print(".")
+	while 1:
+		if GPIO.input(sPin): # button is released
+			print("pew pew")
+			GPIO.output(lPin, GPIO.LOW)
+		else: # button is pressed:
+			print(".")
+			GPIO.output(lPin, GPIO.HIGH)
 except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
-    GPIO.cleanup() # cleanup all GPIO
+	GPIO.cleanup() # cleanup all GPIO
