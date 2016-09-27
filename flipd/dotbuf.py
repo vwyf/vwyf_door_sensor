405d9f5cf38014b3fa0b8d9805728537186e586d
michael philetus weller <philetus@gmail.com>
2016-09-26 18:04:55 -0700

    modified dotbuf to use numpy binary array

--- flipd/dotbuf.py
+++ flipd/dotbuf.py
@@ -1,62 +1,59 @@
 import math
+import numpy as np
+
 import font
 
 class Dotbuf:
 
-    def __init__(self, wdth, hght):
-        self.wdth = wdth
-        self.hght = hght
-        self._b = bytearray(math.ceil(wdth * hght / 8))
+    def __init__(self, wdth=28, hght=7, txt=None, fnt=font.XU):
 
-    def setdot(self, x, y, on): # set byte at offset
-        x = x % self.wdth
-        y = y % self.hght
-        o = (x * 8) + y
-        i = math.floor(o / 8)
-        j = o % 8
-        msk = 0x1 << j
-        if on:
-            self._b[i] |= msk # set bit j of byte i to 1
+        if txt is not None:
+            a = self._textarray(txt, font)
+
+            self.wdth = len(a)
+            self.hght = 7
+            self._b = np.zeros((self.hght, self.wdth), dtype=np.bool)
+            for x in range(wdth):
+                for y in range(hght):
+                    self._b[y, x] = a[x] & (0x1 << y)
+
         else:
-            self._b[i] &= ~msk # set bit j of byte i to 0
+            self.wdth = wdth
+            self.hght = hght
+            self._b = np.zeros((self.hght, self.wdth), dtype=np.bool)
 
-    def getdot(self, x, y):
-        x = x % self.wdth
-        y = y % self.hght
-        o = (x * 8) + y
-        i = math.floor(o / 8)
-        j = o % 8
-        msk = 0x1 << j
-        return self._b[i] & j
+    def __setitem__(self, dex, on):
+        x, y = dex
+        x %= self.wdth
+        y %= self.hght
+        self._b[y, x] = on
 
-    def writedotbuf(self, ox, oy, x, y, w, h, dbf): # write dotbuf values to other
-        for u in range(x, x + w):
-            for v in range(y, y + w):
-                dbf.setdot(ox + u, oy + v, self.getdot(u, v))
+    def __getitem__(self, dex):
+        x, y = dex
+        x %= self.wdth
+        y %= self.hght
+        return self._b[y, x]
 
-    def writeframe(self, ox, oy, frm): # write buffer values to frame at offset
-        for i, x in enumerate(range(ox, ox + frm.wdth)):
+    def writedotbuf(self, othr, ox, oy, x, y, wdth, hght):
+        othr._b[oy:oy+hght, ox:ox+wdth] = self._b[y:y+hght, x:x+wdth]
+
+    def writeframe(self, frm, x, y):
+        for i, x in enumerate(range(x, x + frm.wdth)):
             clmn = 0
-            for i, y in enumerate(range(oy, oy + frm.hght)):
-                if self.getdot(x, y):
+            for i, y in enumerate(range(y, y + frm.hght)):
+                if self[x, y]:
                     clmn |= 0x1 << j
             frm[i] = clmn
 
     def invert(self):
-        for i in range(len(self._b)):
-            self._b[i] = ~self._b[i] & 0xff
+        self._b.invert()
 
-def textbuf(txt, fnt=font.XU):
-    
-    a = bytearray()
-    for c in question_string:
-        if c in font:
-            a.extend(font[c])
-            a.append(0)
+    def _textarray(self, txt, fnt):
+        
+        a = bytearray([0])
+        for c in txt:
+            if c in font:
+                a.extend(font[c])
+                a.append(0)
 
-    b = Dotbuf(len(a) - 1, 7)
-    for x in range(b.wdth):
-        for y in range(7):
-            b.setdot(x, y, a[x] & 0x1 << y)
-
-    return b
+        return a
